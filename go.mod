6a26e14b80774930599c3c3c49c7b2c6a2f0b3a2
Abush Natanem <abushnatanem@gmail.com>
2025-12-17 09:38:40 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module billie
+
+go 1.25.0
---
+++ token/token.go
@@ -0,0 +1,41 @@
+package token;
+
+type TokenType string;
+
+type Token struct {
+    Type  TokenType
+    Literal string
+}
+
+
+
+/// definig our token - names based on our token type
+const (
+
+    // the token which we can't identify
+    ILLEGAL = "ILLEGAL"
+    // which tells end of the file
+    EOF = "EOF"
+
+    IDENT = "IDEN"
+    INT = "INT"
+
+
+    ASSIGN = "="
+    PLUS = "+"
+
+    COMMA = ","
+    SEMICOLON = ";"
+
+    LPAREN = "("
+    RPAREN = ")"
+    LBRACE = "{"
+    RBRACE = "}"
+
+    FUNCTION = "FUNCTION"
+    LET = "LET"
+)
+
+
+
+
